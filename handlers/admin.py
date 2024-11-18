285e4bac0a34c86bd46a1455c46643349d9890c0
bebus <v.gerashchenkoua@gmail.com>
2024-11-18 23:56:06 +0200

    Added disable and enable schedules

--- handlers/admin.py
+++ handlers/admin.py
@@ -32,3 +32,13 @@ async def resume_bot(message: Message):
 async def last_update_bot(message: Message):
     last_update_time: datetime.datetime = datetime.datetime.fromtimestamp(float(await r.get("last_ping_update")))
     await message.answer(last_update_time.strftime("%Y-%m-%d %H:%M:%S"))
+
+@router.message(Command("disable_schedule"))
+async def disable_schedule_bot(message: Message):
+    await r.set("enable_schedule", 0)
+    await message.answer("Schedule disabled!")
+
+@router.message(Command("enable_schedule"))
+async def disable_schedule_bot(message: Message):
+    await r.set("enable_schedule", 1)
+    await message.answer("Schedule enabled!")
