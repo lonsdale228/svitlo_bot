2e70e379a79bb988a221f5e13cea69eb3d711717
bebus <v.gerashchenkoua@gmail.com>
2024-12-02 14:23:33 +0200

    fixed

--- send_request.py
+++ send_request.py
@@ -1,14 +1,23 @@
 import aiohttp
 from config import url_on, url_off
+from loader import logger
 
 
 async def send_on_request():
     async with aiohttp.ClientSession() as session:
         async with session.post(url_on) as response:
-            ...
+            try:
+                data = await response.text()
+                print(data)
+            except Exception as e:
+                logger.error(e)
 
 
 async def send_off_request():
     async with aiohttp.ClientSession() as session:
         async with session.post(url_off) as response:
-            ...
+            try:
+                data = await response.text()
+                print(data)
+            except Exception as e:
+                logger.error(e)
