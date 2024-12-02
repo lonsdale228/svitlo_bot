2638a336765da84b019589fdff03362eee81682c
bebus <v.gerashchenkoua@gmail.com>
2024-12-02 13:32:31 +0200

    fixed

--- send_request.py
+++ send_request.py
@@ -5,18 +5,10 @@ from config import url_on, url_off
 async def send_on_request():
     async with aiohttp.ClientSession() as session:
         async with session.post(url_on) as response:
-            if response.status == 200:
-                data = await response.json()
-                print("Response:", data)
-            else:
-                print(f"Request failed with status {response.status}")
+            ...
 
 
 async def send_off_request():
     async with aiohttp.ClientSession() as session:
         async with session.post(url_off) as response:
-            if response.status == 200:
-                data = await response.json()  # Parse JSON response
-                print("Response:", data)
-            else:
-                print(f"Request failed with status {response.status}")
+            ...
