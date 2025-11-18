0f81f3bb869a4b1e91f84fa61965710711770d3b
lonsdale228 <v.gerashchenkoua@gmail.com>
2025-11-18 16:17:04 +0200

    fixed sending request

--- bot.py
+++ bot.py
@@ -221,7 +221,7 @@ async def msg_editor(b: Bot, lock):
     tz_info_off_time = off_time.tzinfo
     tz_info_on_time = on_time.tzinfo
 
-    prev_timetable = bool(await r.get("prev_timetable", 0))
+    prev_timetable = bool(await r.get("prev_timetable"))
 
     if not prev_timetable and ranges_tomorrow:
         await b.send_message(chat_id=MY_ID, text="Додано графіки на завтра!")
